3d01b1861339cfe8bcdda2737e312211d68b628e
Эдгар <108433596+hayk4500@users.noreply.github.com>
2022-07-08 17:22:16 +0300

    SimpleCalc — Простой калькулятор
    Простой калькулятор, выполняющий четыре арифметических операций (Сложение, Вычитание, Умножение и Разделение). Реализовано с помощью функции.

---
+++ SimpleCalc.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double Add(double, double);
+double Sub(double, double);
+double Mult(double, double);
+double Div(double, double);
+
+int main() {
+	double a, b, c;
+	char op;
+	do {
+		cout << "Enter the numbers and operand beetween them: "; cin >> a >> op >> b;
+		switch (op) {
+		case '+':c = Add(a, b); break;
+		case '-':c = Sub(a, b); break;
+		case '*':c = Mult(a, b); break;
+		case '/':c = Div(a, b); break;
+		default: cout << "Wrong operand! Try again!" << endl; continue;
+		}
+		cout << "Result: " << c << endl;
+	} while (a && b != 0);
+	return 0;
+}
+
+double Add(double x, double y) {
+	double z = x + y;
+	return z;
+}
+double Sub(double x, double y) {
+	double z = x - y;
+	return z;
+}
+double Mult(double x, double y) {
+	double z = x * y;
+	return z;
+}
+double Div(double x, double y) {
+	double z = x / y;
+	return z;
+}
